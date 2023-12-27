f2d88a99d13877b18072a8a2539db3fd99be4d38
hector7178 <hjgrimanmarcano@gmail.com>
2023-12-27 01:04:28 -0400

    ready

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const server = require("http").createServer(app);
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 const qrcode = require("qrcode");
 
 
