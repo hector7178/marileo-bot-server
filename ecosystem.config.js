4edb9272412921779c78f66e8693bb3797284607
hector7178 <hjgrimanmarcano@gmail.com>
2023-12-27 00:51:46 -0400

    ready

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       host : '15.228.57.23',
       ref  : 'origin/main',
       repo : 'https://github.com/hector7178/marileo-bot-server.git',
-      path : '/home/ubuntu',
+      path : '/home/ubuntu/server',
       'pre-deploy-local': '',
       'post-deploy' : 'source ~/nvm/nvm.sh && npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
