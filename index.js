6d14d627947490f4e2b177308cd89a053bff4f33
hector7178 <76874922+hector7178@users.noreply.github.com>
2024-06-12 21:35:28 -0400

    Update hora index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ const isConnected = () => {
   return sock?.user ? true : false;
 };
 
-cron.schedule('30 18 * * *', () => {
+cron.schedule('00 22 * * *', () => {
   axios.get('https://gestioncuentas.shop/api/accounts').then(function (response) {
     
     const data=response.data["suscription"].filter((data)=>{
